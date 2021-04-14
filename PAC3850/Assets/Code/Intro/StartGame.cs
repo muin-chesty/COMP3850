3aea23c1b5b65963569313749f4055df45b6aa43
muin-unity-comp111 <53714737+muin-chesty@users.noreply.github.com>
2021-04-14 19:35:14 +1000

    Animation added + Kaden's work has been integrated

---
+++ PAC3850/Assets/Code/Intro/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    private float timer = 0f;
+    [Tooltip("Number of seconds the level will wait until going to the next level")]
+    [SerializeField]
+    private float startsIn = 2f;
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= startsIn)
+        {
+            timer = 0f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
