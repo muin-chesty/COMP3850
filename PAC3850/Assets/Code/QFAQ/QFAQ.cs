98db62413c62ab0efc02c1f3c398b60eaac10c3c
muin-unity-comp111 <53714737+muin-chesty@users.noreply.github.com>
2021-04-29 12:38:26 +1000

    Parents UI started

---
+++ PAC3850/Assets/Code/QFAQ/QFAQ.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class QFAQ : MonoBehaviour
+{
+    public void LoadFAQ()
+    {
+        SceneManager.LoadScene("ChildInfo");
+    }
+
+    public void LoadAsk()
+    {
+        SceneManager.LoadScene("ParentInfo");
+    }
+}
