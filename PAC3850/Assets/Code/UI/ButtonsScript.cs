59b1eed01459a47c21913d13f0d29da30427a703
muin-unity-comp111 <53714737+muin-chesty@users.noreply.github.com>
2021-05-08 21:50:38 +1000

    ParentInfo Level has image, text fields and buttons working

---
+++ PAC3850/Assets/Code/UI/ButtonsScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsScript : MonoBehaviour
+{
+    private const string PARENT_MENU = "ParentMenu";
+
+    [SerializeField]
+    private UnityEngine.UI.InputField firstName;
+    [SerializeField]
+    private UnityEngine.UI.InputField id;
+
+    private void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    private void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    // THIS FUNCTION SHOULD BE CALLED FROM THE PLAY BUTTON ON PARENT INFO LEVEL
+    public void ParentInfoButton()
+    {
+        if(firstName.text.Length > 0 && id.text.Length > 0)
+        {
+            LoadLevel(PARENT_MENU);
+        }
+        
+    }
+
+}
