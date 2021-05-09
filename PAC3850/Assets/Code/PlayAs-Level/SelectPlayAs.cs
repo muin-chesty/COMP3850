0abcfd1e9c542d11f7283135e2bead5e5224006b
muin-unity-comp111 <53714737+muin-chesty@users.noreply.github.com>
2021-05-09 14:59:10 +1000

    Play As level completed. Parent Info completed

--- PAC3850/Assets/Code/PlayAs-Level/SelectPlayAs.cs
+++ PAC3850/Assets/Code/PlayAs-Level/SelectPlayAs.cs
@@ -1,17 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SelectPlayAs : MonoBehaviour
 {
+    [SerializeField]
+    UnityEngine.UI.Text playAs;
+    [SerializeField]
+    GameObject outroObject;
+
+    private float initTimer = 0.0f;
+    private float initDelay = 0.5f;
+    private float timer = 0.0f;
+    [SerializeField]
+    private float pause = 0.1f;
+    private int index = 0;
+    private char[] text = { 'P', 'L', 'A', 'Y', ' ', 'A', 'S' };
+
+    private bool isButtonClicked = false;
+    private float levelTimer = 0.0f;
+    private float levelDelay = 1f;
+    private string levelName = "";
+
+    private void Start()
+    {
+        outroObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if(isButtonClicked)
+        {
+            levelTimer += Time.deltaTime;
+            outroObject.SetActive(true);
+            if(levelTimer >= levelDelay)
+            {
+                SceneManager.LoadScene(levelName);
+            }
+        }
+        initTimer += Time.deltaTime;
+        if(initTimer >= initDelay)
+        {
+            timer += Time.deltaTime;
+            if (timer >= pause)
+            {
+                timer = 0.0f;
+                if (index < text.Length)
+                {
+                    playAs.text += text[index++];
+                }
+
+            }
+        }
+
+    }
     public void LoadChildSection()
     {
-        SceneManager.LoadScene("ChildInfo");
+        isButtonClicked = true;
+        levelName = "ChildInfo";
     }
 
     public void LoadParentSection()
     {
-        SceneManager.LoadScene("ParentInfo");
+        isButtonClicked = true;
+        levelName = "ParentInfo";
     }
 }
--- PAC3850/Assets/Code/UI/ButtonsScript.cs
+++ PAC3850/Assets/Code/UI/ButtonsScript.cs
@@ -5,14 +5,77 @@ public class ButtonsScript : MonoBehaviour
 {
     private const string PARENT_MENU = "ParentMenu";
 
+
     [SerializeField]
     private UnityEngine.UI.InputField firstName;
     [SerializeField]
     private UnityEngine.UI.InputField id;
+    [SerializeField]
+    private GameObject firstNameInput;
+    [SerializeField]
+    private GameObject idInput;
+    [SerializeField]
+    private GameObject outroGameObject;
 
+
+
+    private Animator anim;          // USED TO CONTROL FIRST NAME INPUT FIELD'S ANIMATION
+    private Animator idAnimator;    // USED TO CONTROL ID INPUT FIELD'S ANIMATION
+ 
+
+    private float timer = 0.0f;     // USED TO RESET FIRST NAME'S BOOL
+    private float timerOne = 0.0f;  // USED TO RESET ID'S BOOL
+    private float waitForNSeconds = 0.2f;  // USED TO WAIT BEFORE BOOL VALUE RESETS
+
+    private float timerForLevelTranisition = 0.0f;   // TO BE USED TO MEASURE TIME BETWEEN BUTTON CLICK AND LEVEL TRANSITION PAUSE
+    private float transitionPauseInSeconds = 1f;     // NUMBER OF SECONDS BEFORE THE NEXT LEVEL LOADS
+
+    private bool isLevelComplete = false;
+
+    private void Start()
+    {
+        anim = firstNameInput.transform.GetComponent<Animator>();
+        idAnimator = idInput.transform.GetComponent<Animator>();
+        outroGameObject.SetActive(false);
+       
+    }
+    private void Update()
+    {
+        if(isLevelComplete)
+        {
+            timerForLevelTranisition += Time.deltaTime;
+            if(timerForLevelTranisition >= transitionPauseInSeconds)
+            {
+                timerForLevelTranisition = 0.0f;
+                isLevelComplete = false;
+                LoadLevel(PARENT_MENU);
+            }
+        }
+
+        if(anim.GetBool("isEmpty") == true)
+        {
+            timer += Time.deltaTime;
+            if(timer >= waitForNSeconds)
+            {
+                timer = 0.0f;
+                anim.SetBool("isEmpty", false);
+            }
+        }
+
+        if (idAnimator.GetBool("isEmpty") == true)
+        {
+            timerOne += Time.deltaTime;
+            if (timerOne >= waitForNSeconds)
+            {
+                timerOne = 0.0f;
+                idAnimator.SetBool("isEmpty", false);
+            }
+        }
+    }
     private void LoadLevel(string name)
     {
         SceneManager.LoadScene(name);
+        
     }
     private void NextLevel()
     {
@@ -23,7 +86,23 @@ public class ButtonsScript : MonoBehaviour
     {
         if(firstName.text.Length > 0 && id.text.Length > 0)
         {
-            LoadLevel(PARENT_MENU);
+            outroGameObject.SetActive(true);
+            isLevelComplete = true;
+        }
+        else
+        {
+            if(firstName.text.Length <= 0)
+            {
+                anim.SetBool("isEmpty", true);
+               
+            }
+
+            if (id.text.Length <= 0)
+            {
+                idAnimator.SetBool("isEmpty", true);
+                
+
+            }
         }
         
     }
