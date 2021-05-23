d9a51e771062dc6e0d1d2dfae65fda1ea7be880d
Oakaidden <54323962+Oakaidden@users.noreply.github.com>
2021-05-24 02:46:12 +1000

    Height and Weight Code Complete
    Height and Weight Code has been complete it just needs art because it is currently just a lot of squares

---
+++ PAC3850/Assets/Scenes/Child/Kaden/Code H&W/GoToNextLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToNextLevel : MonoBehaviour
+{
+    public Renderer Continue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartNextLevel()
+    {
+        if (Continue.enabled == true)
+        {
+            Debug.Log("LOAD NEXT LEVEL HERE");
+        }
+    }
+}
---
+++ PAC3850/Assets/Scenes/Child/Kaden/Code H&W/ObjVis.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjVis : MonoBehaviour
+{
+
+    public Renderer CharOnScale;
+    public Renderer ScaleCheckmark;
+    public Renderer ScaleTip;
+    public Renderer ScaleTipArrow;
+    public Text ScaleTipText;
+    public Renderer CharToMeasure;
+    public Renderer MeasureCheckmark;
+    public Renderer MeasureTip;
+    public Renderer MeasureTipArrow;
+    public Text MeasureTipText;
+    public Renderer Continue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ScaleCheckmark.enabled == true && MeasureCheckmark.enabled == true)
+        {
+            Continue.enabled = true;
+        }
+    }
+
+    public void OnScale()
+    {
+        if(ScaleCheckmark.enabled != true)
+        {
+            if (CharToMeasure.enabled == true)
+            {
+                CharToMeasure.enabled = false;
+            }
+            ScaleTipText.enabled = false;
+            ScaleTip.enabled = false;
+            ScaleTipArrow.enabled = false;
+            CharOnScale.enabled = true;
+            ScaleCheckmark.enabled = true;
+        }
+    }
+
+    public void ToMeasure()
+    {
+        if(MeasureCheckmark.enabled != true)
+        {
+            if (CharOnScale.enabled == true)
+            {
+                CharOnScale.enabled = false;
+            }
+            MeasureTipText.enabled = false;
+            MeasureTip.enabled = false;
+            MeasureTipArrow.enabled = false;
+            CharToMeasure.enabled = true;
+            MeasureCheckmark.enabled = true;
+        }
+    }
+}
