86f9f701d3c0c54bae1af2ba03300658cf7848da
Oakaidden <54323962+Oakaidden@users.noreply.github.com>
2021-06-02 06:43:44 +1000

    ECG nearly done

---
+++ PAC3850/Assets/Scenes/Child/Kaden/Code ECG/ecg1TOecg2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ecg1TOecg2 : MonoBehaviour
+{
+    public Renderer Continue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextLevel()
+    {
+        if(Continue.enabled == true)
+        {
+            SceneManager.LoadScene("ECG2");
+        }
+    }
+}
---
+++ PAC3850/Assets/Scenes/Child/Kaden/Code ECG/shirtOff.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shirtOff : MonoBehaviour
+{
+
+    public Renderer shirtOn;
+    public Renderer shirtless;   
+    public Renderer Continue;
+    public Renderer tipWindow;
+    public Renderer tipArrow;
+    public Renderer checkmark;
+    public Text tip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeShirtOff()
+    {
+        shirtOn.enabled = false;
+        shirtless.enabled = true;
+        Continue.enabled = true;
+        tipWindow.enabled = false;
+        tipArrow.enabled = false;
+        tip.enabled = false;
+        checkmark.enabled = true;
+    }
+}
