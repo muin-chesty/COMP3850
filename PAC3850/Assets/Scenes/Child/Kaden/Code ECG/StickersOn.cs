4bc159ce66fe490f8f4e0de4f5d8684de31d2495
Oakaidden <54323962+Oakaidden@users.noreply.github.com>
2021-06-02 07:08:01 +1000

    ECG done just needs the art stuff

---
+++ PAC3850/Assets/Scenes/Child/Kaden/Code ECG/StickersOn.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickersOn : MonoBehaviour
+{
+
+    public Renderer chest;
+    public Renderer arm;
+    public Renderer leg;
+    public Renderer chestWire;
+    public Renderer armWire;
+    public Renderer legWire;
+    public Renderer monitorCov1;
+    public Renderer monitorCov2;
+    public Renderer monitorCov3;
+    public Renderer monitorCov4;
+    public Renderer monitorCov5;
+    public Renderer monitorCov6;
+    public Renderer monitorCov7;
+    public Renderer Continue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(leg.enabled == true && arm.enabled == true && chest.enabled == true)
+        {
+            chestWire.enabled = true;
+            armWire.enabled = true;
+            legWire.enabled = true;
+            monitorCov1.enabled = false;
+            monitorCov2.enabled = false;
+            monitorCov3.enabled = false;
+            monitorCov4.enabled = false;
+            monitorCov5.enabled = false;
+            monitorCov6.enabled = false;
+            monitorCov7.enabled = false;
+            Continue.enabled = true;
+        }
+    }
+
+    public void legSticker()
+    {
+        leg.enabled = true;
+    }
+
+    public void chestSticker()
+    {
+        chest.enabled = true;
+    }
+
+    public void armSticker()
+    {
+        arm.enabled = true;
+    }
+}
